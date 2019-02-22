028cc2a0eed99bc3c24550dd096bfb8a8f1e4d31
Malte Muth <malte.muth+git-commit@googlemail.com>
2019-02-22 19:29:56 +0100

    [FIX] handle URLs with # and ? correctly

--- src/url_replacer.js
+++ src/url_replacer.js
@@ -1,14 +1,20 @@
 const path = require('path');
 
-module.exports = ({str, abs = 0}) =>
-    str.replace(/url\(['"]?([\w-_ ./]*\.[\w-_ ]*)['"]?\)/g, (m,file) =>
-        `url("\$CMS_REF(media:"${path.parse(file).name.replace(/(-| |\.)/g, (m) => {
-            switch(m) {
-            case ' ':
-            case '.':
-                return '';
-            case '-':
-                return '_';
-            }
-        })}", abs: ${abs})$")`
-);
+module.exports = ({ str, abs = 0 }) =>
+    str.replace(
+        /url\(['"]?([\w-_ ./]*\.[\w-_ ]*)([?#a-zA-Z]*)['"]?\)/g,
+        (matches, file, suffix) =>
+            `url("$CMS_REF(media:"${path
+                .parse(file)
+                .name.replace(/(-| |\.)/g, (m) => {
+                    switch (m) {
+                    case ' ':
+                    case '.':
+                        return '';
+                    case '-':
+                        return '_';
+                    default:
+                        return '';
+                    }
+                })}", abs: ${abs})$${suffix}")`
+    );
